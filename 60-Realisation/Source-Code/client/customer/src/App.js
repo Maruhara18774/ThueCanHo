3e6b52848994d275b77d6d512260a7209c7f8e7f
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-04-08 20:11:07 +0700

    8/4/2021

---
+++ 60-Realisation/Source-Code/client/customer/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './views/Login'
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
