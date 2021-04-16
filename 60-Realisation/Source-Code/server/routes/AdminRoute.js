189a6c8a0ffe1397774bad6c9a0cf50946cbd384
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-04-16 11:47:11 +0700

    Clear old sources

--- 60-Realisation/Source-Code/server/routes/AdminRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require("../controllers/Admin/AdminController")
-
-
-router.get("/",controller.get)
-
-module.exports = router;
