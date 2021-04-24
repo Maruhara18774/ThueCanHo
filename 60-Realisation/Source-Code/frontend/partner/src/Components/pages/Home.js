9cca4ea6ae95abd0f478adad275ca64b631768b8
phuongThao460 <blackcompany1998@gmail.com>
2021-04-24 16:48:31 +0700

    Initial commit

---
+++ 60-Realisation/Source-Code/frontend/partner/src/Components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Cards from '../paner-form/Cards';
+import Navbar from '../paner-form/Navbar';
+import '../../style/pages/Home.css'
+function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Cards />
+      
+    </>
+  );
+}
+
+export default Home;
