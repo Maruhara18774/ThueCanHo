d6cfd93bfd52afd4ede8d977900bde1997acf805
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-04-25 22:57:17 +0700

    Navbar basic finish

--- 60-Realisation/Source-Code/frontend/customer/src/Components/nav-bar.js
+++ 60-Realisation/Source-Code/frontend/customer/src/Components/nav-bar.js
@@ -4,11 +4,14 @@ import './nav-bar.css'
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="bg-main-nav navbar navbar-expand-lg navbar-dark">
       <Link to="/">
         <div class="navbar-brand">
-          <img class="crb-logo" src="https://www.flaticon.com/svg/vstatic/svg/201/201623.svg?token=exp=1619342866~hmac=e432d53791660245e83a80fbf2cc5a63" width="30" height="30" class="d-inline-block align-top" alt="logo" />
-                    Cabeanloka
+        <h3>
+          <span class="mrl10">
+          <img src="https://www.flaticon.com/svg/vstatic/svg/201/201623.svg?token=exp=1619342866~hmac=e432d53791660245e83a80fbf2cc5a63" width="30" height="30" class="d-inline-block align-top" alt="logo" />
+          </span>
+                    Cabeanloka</h3>
                 </div>
       </Link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,11 +19,19 @@ function Navbar() {
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link to="/apaLst" className="white-text mrl10"><span className="mrl10"><i class="fas fa-home"></i></span>Xem căn hộ</Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/apaLst" className="white-text mrl10"><span className="mrl10"><i class="fas fa-chart-line"></i></span>Trending</Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/apaLst" className="white-text mrl10"><span className="mrl10"><i class="fas fa-gift"></i></span>Khuyến mãi</Link>
+            </li>
+          </ul>
+        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
           <li class="nav-item active">
-            <Link to="/login">Login</Link>
-          </li>
-          <li class="nav-item">
-            <Link to="/apaLst">Apartment List</Link>
+            <Link to="/login" className="white-text"><span className="mrl10"><i class="far fa-user"></i></span>Login</Link>
           </li>
         </ul>
       </div>
