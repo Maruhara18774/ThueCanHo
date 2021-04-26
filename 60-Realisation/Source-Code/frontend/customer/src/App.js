9a73f524f841a5f1f3e562d00dfcf9754bab0646
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-04-26 21:23:21 +0700

    Footer done not link

--- 60-Realisation/Source-Code/frontend/customer/src/App.js
+++ 60-Realisation/Source-Code/frontend/customer/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
 import Login from './Components/login.js';
 import ListApartment from './Components/show-apartment';
 import HomePage from './Pages/home'
+import FooterCustom from './Components/footer'
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
           <Route path="/" exact component={HomePage}></Route>
         </Switch>
       </Router>
+      <FooterCustom/>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     </div>
