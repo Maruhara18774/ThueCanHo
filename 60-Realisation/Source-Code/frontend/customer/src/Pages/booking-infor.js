9a6aeeeb57d9d3918ee5b6479d9f73f09c4ba72d
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-05-22 18:52:14 +0700

    [Payment] Day and time checkin checkout

--- 60-Realisation/Source-Code/frontend/customer/src/Pages/booking-infor.js
+++ 60-Realisation/Source-Code/frontend/customer/src/Pages/booking-infor.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './booking-infor.css';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 export default class BookingForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
             idApartment: document.location.pathname.substring(9),
+            apartmentInfo: {},
             currentStep: 1,
             // TAIKHOAN - Tich diem
             username: "",
@@ -27,9 +29,11 @@ export default class BookingForm extends Component {
             checkOut: "",
             soBuaSang: 0,
             soGiuongPhu: 0,
-        }
 
+        }
+        this.getApartmentInfo();
     }
+    // Action in form
     nextStep = (current) => {
         this.state.currentStep = current + 1;
         this.setState(this);
@@ -42,6 +46,7 @@ export default class BookingForm extends Component {
     }
     componentDidMount(){
         window.scrollTo(0,0);
+        this.state.ngayDen = this.getDateNow();
     }
     confirmEmail = () =>{
         if(!this.state.email.includes("@")||this.state.email ==""){
@@ -56,7 +61,27 @@ export default class BookingForm extends Component {
             {return false;}
         return true;
     }
-    checkInput = () =>{
+    confirmTimeInput = (time)=>{
+        if(time==""){
+            return false;
+        }
+        const pointIn = this.state.apartmentInfo.CHECKIN.substring(11,16);
+        const pointOut = this.state.apartmentInfo.CHECKOUT.substring(11,16);
+        const hour = time.substring(0,2);
+        const minutes = time.substring(3,5);
+        if(parseInt(hour)<parseInt(pointIn.substring(0,2))||parseInt(hour)>parseInt(pointOut.substring(0,2))){
+            return false;
+        }
+        if(parseInt(hour)==parseInt(pointIn.substring(0,2))&&parseInt(minutes)<parseInt(pointIn.substring(3,5))){
+            return false;
+        }
+        if(parseInt(hour)==parseInt(pointOut.substring(0,2))&&parseInt(minutes)>parseInt(pointOut.substring(3,5))){
+            return false;
+        }
+        return true;
+    }
+    checkInput1 = () =>{
+        /*
         if(this.state.tenKH==""||
         this.state.email ==""||
         !this.state.email.includes("@")||
@@ -65,8 +90,21 @@ export default class BookingForm extends Component {
         this.state.quocTich==""){
             return false;
         }
+        */
         return true;
     }
+    checkInput2 = () =>{
+        if(this.state.ngayDen==""||
+        this.state.ngayDi==""||
+        this.state.checkIn==""||
+        this.state.checkOut==""||
+        !this.confirmTimeInput(this.state.checkIn)||
+        !this.confirmTimeInput(this.state.checkOut)){
+            return false;
+        }
+        return true;
+    }
+    // Input state
     setTenKH = (event) =>{
         this.state.tenKH=event.target.value;
         this.setState(this);
@@ -95,7 +133,42 @@ export default class BookingForm extends Component {
         this.state.gioiTinh=event.target.value;
         this.setState(this);
     }
+
+    setNgayDen = (event) =>{
+        this.state.ngayDen = event.target.value;
+        this.setState(this);
+    }
+    setNgayDi = (event) =>{
+        this.state.ngayDi = event.target.value;
+        this.setState(this);
+    }
+    setCheckin = (event) =>{
+        this.state.checkIn = event.target.value;
+        this.setState(this);
+    }
+    setCheckout = (event) =>{
+        this.state.checkOut = event.target.value;
+        this.setState(this);
+    }
+    getDateNow = ()=>{
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
+        var yyyy = today.getFullYear();
+        return yyyy+"-"+mm+"-"+dd;
+    }
+    // Get start data from server
+    getApartmentInfo = ()=>{
+        Axios.post("http://localhost:33456/api/customer/getDetailApartment",{id: this.state.idApartment})
+        .then(response=>{
+            this.state.apartmentInfo = response.data[0];
+            this.setState(this);
+        })
+        
+    }
+
     render() {
+        
         switch (this.state.currentStep) {
             case 1:
                 return (
@@ -202,7 +275,7 @@ export default class BookingForm extends Component {
                                         </div>
                                 </form>
                                 <br/>
-                                {this.checkInput()?
+                                {this.checkInput1()?
                                 <button type="button" class="btn btn-warning btn-lg" onClick={() => this.nextStep(this.state.currentStep)}>Bước tiếp theo</button>:
                                 <button type="button" class="btn btn-secondary btn-lg" disabled>Hãy điền đủ thông tin ...</button>}
                             </div>
@@ -239,9 +312,52 @@ export default class BookingForm extends Component {
                                     </tr>
                                 </table>
                             </div>
-                            Bước 2
-                            <button onClick={() => this.prevStep(this.state.currentStep)}>Previous</button>
-                            <button onClick={() => this.nextStep(this.state.currentStep)}>Next</button>
+                            <hr/>
+                            <div className="inputZone">
+                                <p className="title">Thông tin thuê</p>
+                                <form>
+                                    <div  className="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="FMBookingDayFrom">Ngày đến:</label>
+                                            <input type="date" class="form-control is-valid" id="FMBookingDayFrom" min={this.getDateNow()} value={this.getDateNow()} onChange={this.setNgayDen} required />
+                                            </div>
+                                        {(this.state.ngayDi!="")&&(this.state.ngayDen=="")?
+                                        <div class="form-group col-md-6">
+                                            <label for="FMBookingDayTo">Ngày đi</label>
+                                            <input type="date" class="form-control is-valid" id="FMBookingDayTo" min={this.state.ngayDen} onChange={this.setNgayDi} required />
+                                            </div>
+                                            : <div class="form-group col-md-6">
+                                                <label for="FMBookingDayTo">Ngày đi</label>
+                                            <input type="date" class="form-control" id="FMBookingDayTo" min={this.state.ngayDen} onChange={this.setNgayDi} required />
+                                            </div>}
+                                    </div>
+                                    <div  className="form-row">
+                                        {this.confirmTimeInput(this.state.checkIn)?
+                                        <div class="form-group col-md-6">
+                                            <label for="FMBookingTimeFrom">Checkin:</label>
+                                            <input type="time" class="form-control is-valid" id="FMBookingTimeFrom" required />
+                                            </div>
+                                            : <div class="form-group col-md-6">
+                                                <label for="FMBookingTimeFrom">Checkin:</label>
+                                            <input type="time" class="form-control" id="FMBookingTimeFrom" required />
+                                            </div>}
+                                        {this.confirmTimeInput(this.state.checkOut)?
+                                        <div class="form-group col-md-6">
+                                            <label for="FMBookingTimeTo">Checkout</label>
+                                            <input type="time" class="form-control is-valid" id="FMBookingTimeTo" required />
+                                            </div>
+                                            : <div class="form-group col-md-6">
+                                                <label for="FMBookingTimeTo">Checkout</label>
+                                            <input type="time" class="form-control" id="FMBookingTimeTo" onChange={this.setCheckout} required />
+                                            </div>}
+                                    </div>
+                                        
+                                </form>
+                                <br/>
+                                {this.checkInput2()?
+                                <button type="button" class="btn btn-warning btn-lg" onClick={() => this.nextStep(this.state.currentStep)}>Bước tiếp theo</button>:
+                                <button type="button" class="btn btn-secondary btn-lg" disabled>Hãy điền đủ thông tin ...</button>}
+                            </div>
                         </div>
                     </div>
                 )
