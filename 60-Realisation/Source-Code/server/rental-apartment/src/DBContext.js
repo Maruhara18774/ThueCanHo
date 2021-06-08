4cdc4ec7ff6f9ea5062782ca979336465d07effe
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-06-08 14:43:15 +0700

    Update DBContext.js

--- 60-Realisation/Source-Code/server/rental-apartment/src/DBContext.js
+++ 60-Realisation/Source-Code/server/rental-apartment/src/DBContext.js
@@ -3,11 +3,11 @@ const {Sequelize} = require('sequelize');
 
 const userDBOptions = {
     dialect: 'mssql',
-    host: 'SQL5091.site4now.net',
-    port: '1433',
-    database: 'db_a752df_rentalapartment',
-    username: 'db_a752df_rentalapartment_admin',
-    password: 'thu3c@nh0'
+    host: 'localhost',
+    port: '1400',
+    database: 'RENTALAPARTMENT',
+    username: 'sa',
+    password: '!Passw0rd'
 }
 /*
 class DatabaseContext{
